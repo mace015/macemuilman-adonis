2bdc9c3b00df42e5f62852f4355d3ef30e7c1994
Mace Muilman <mace@macemuilman.nl>
2016-10-17 21:43:09 +0200

    Added validation for the contact form.

--- app/Http/Controllers/ContactController.js
+++ app/Http/Controllers/ContactController.js
@@ -1,10 +1,18 @@
 'use strict'
 
 const Mail = use('Mail')
+const Validator = use('Validator')
 
 class ContactController {
 
 	* versturen (request, response) {
+
+		const rules = {
+			'naam': 'required',
+			'email': 'required',
+			'onderwerp': 'required',
+			'bericht': 'required'
+		}
 
 		const input = {
 			'naam': request.input('naam'),
@@ -12,6 +20,12 @@ class ContactController {
 			'onderwerp': request.input('onderwerp'),
 			'bericht': request.input('bericht')
 		};
+
+		const validation = yield Validator.validate(input, rules);
+		if (validation.fails()) {
+			yield request.with({ errors: validation.messages() }).flash();
+			return response.route('contact');
+		}
 
 		yield Mail.send('emails.contact', input, (message) => {
 			message.to('mace@macemuilman.nl', 'Mace Muilman');
